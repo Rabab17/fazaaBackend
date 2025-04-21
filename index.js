d5166849557a13066e6715a075da3e3274e5b01b
rabab <rababalaa1761999@gmail.com>
2025-04-21 20:21:42 +0200

    rabab

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+require("dotenv").config();
+
+  const PORT = process.env.PORT || 3000
+  server.listen(PORT, () => {
+    connect();
+    console.log(`Server running on port ${PORT}`);
+  });
+  
+  const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("MongoDB connection error:", error.message);
+    }
+  };
+  
